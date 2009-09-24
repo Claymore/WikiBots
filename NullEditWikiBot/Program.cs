33d35ecdd97474ca111eb7da2a1e03e57bc87a9a
Alexey Bobyakov <claymore.ws@gmail.com>
2009-09-24 20:35:58 +0400

    fail-safe and script friendly version

--- NullEditWikiBot/Program.cs
+++ NullEditWikiBot/Program.cs
@@ -3,19 +3,20 @@ using System.Xml;
 using Claymore.NullEditWikiBot.Properties;
 using Claymore.SharpMediaWiki;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Claymore.NullEditWikiBot
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Wiki wiki = new Wiki("http://ru.wikipedia.org");
             if (string.IsNullOrEmpty(Settings.Default.Login) ||
                 string.IsNullOrEmpty(Settings.Default.Password))
             {
                 Console.Out.WriteLine("Please add login and password to the configuration file.");
-                return;
+                return -1;
             }
 
             Console.Out.WriteLine("Logging in as " + Settings.Default.Login + "...");
@@ -40,9 +41,26 @@ namespace Claymore.NullEditWikiBot
             catch (WikiException e)
             {
                 Console.Out.WriteLine(e.Message);
-                return;
+                return -1;
             }
             Console.Out.WriteLine("Logged in as " + Settings.Default.Login + ".");
+
+            string errorFileName = @"Errors.txt";
+            if (!File.Exists(errorFileName))
+            {
+                using (FileStream stream = File.Create(errorFileName)) { }
+            }
+
+            int lastIndex = 0;
+            int taskIndex = 0;
+            using (TextReader streamReader = new StreamReader(errorFileName))
+            {
+                string line = streamReader.ReadToEnd();
+                if (!string.IsNullOrEmpty(line))
+                {
+                    lastIndex = int.Parse(line);
+                }
+            }
 
             Dictionary<string, string> templates = new Dictionary<string, string>();
             templates.Add("Шаблон:Deleteslow", "Категория:Википедия:К быстрому удалению");
@@ -54,13 +72,73 @@ namespace Claymore.NullEditWikiBot
             ParameterCollection parameters = new ParameterCollection();
             foreach (var template in templates)
             {
+                if (lastIndex <= taskIndex)
+                {
+                    parameters.Clear();
+                    parameters.Add("generator", "embeddedin");
+                    parameters.Add("geititle", template.Key);
+                    parameters.Add("geilimit", "max");
+                    parameters.Add("geinamespace", "0");
+                    parameters.Add("prop", "categories");
+                    parameters.Add("clcategories", template.Value);
+
+                    try
+                    {
+                        doc = wiki.Enumerate(parameters, true);
+                    }
+                    catch (WikiException e)
+                    {
+                        Console.Out.WriteLine(e.Message);
+                        using (TextWriter streamWriter = new StreamWriter(errorFileName))
+                        {
+                            streamWriter.Write(taskIndex);
+                        }
+                        return -1;
+                    }
+
+                    pages = doc.SelectNodes("//page");
+                    index = 1;
+                    foreach (XmlNode page in pages)
+                    {
+                        XmlNode category = page.SelectSingleNode("categories/cl");
+                        if (category != null)
+                        {
+                            continue;
+                        }
+                        string pageTitle = page.Attributes["title"].Value;
+                        Console.Out.WriteLine(string.Format("Processing '{0}' ({1}/{2})...",
+                            pageTitle, index++, pages.Count));
+                        try
+                        {
+                            wiki.AppendTextToPage(pageTitle,
+                                "\n\n",
+                                "Сброс кеша нулевой правкой",
+                                MinorFlags.None,
+                                WatchFlags.Watch);
+                        }
+                        catch (WikiException e)
+                        {
+                            Console.Out.WriteLine(e.Message);
+                            using (TextWriter streamWriter = new StreamWriter(errorFileName))
+                            {
+                                streamWriter.Write(taskIndex);
+                            }
+                            return -1;
+                        }
+                    }
+                    ++taskIndex;
+                }
+            }
+
+            if (lastIndex <= taskIndex)
+            {
                 parameters.Clear();
                 parameters.Add("generator", "embeddedin");
-                parameters.Add("geititle", template.Key);
+                parameters.Add("geititle", "Шаблон:Orphaned-fairuse");
                 parameters.Add("geilimit", "max");
-                parameters.Add("geinamespace", "0");
+                parameters.Add("geinamespace", "6");
                 parameters.Add("prop", "categories");
-                parameters.Add("clcategories", template.Value);
+                parameters.Add("clcategories", "Категория:Файлы:Неиспользуемые несвободные:Просроченные подведения итогов");
 
                 try
                 {
@@ -69,7 +147,7 @@ namespace Claymore.NullEditWikiBot
                 catch (WikiException e)
                 {
                     Console.Out.WriteLine(e.Message);
-                    return;
+                    return -1;
                 }
 
                 pages = doc.SelectNodes("//page");
@@ -95,100 +173,76 @@ namespace Claymore.NullEditWikiBot
                     catch (WikiException e)
                     {
                         Console.Out.WriteLine(e.Message);
+                        break;
+                    }
+                }
+
+                ++taskIndex;
+            }
+
+            if (lastIndex <= taskIndex)
+            {
+                parameters.Clear();
+                parameters.Add("generator", "categorymembers");
+                parameters.Add("gcmtitle", "Категория:Файлы:Перенесённые ботом");
+                parameters.Add("gcmlimit", "max");
+                parameters.Add("gcmnamespace", "6");
+                parameters.Add("prop", "categories");
+                parameters.Add("clcategories", "Категория:Файлы:Перенесённые на Викисклад");
+
+                try
+                {
+                    doc = wiki.Enumerate(parameters, true);
+                }
+                catch (WikiException e)
+                {
+                    Console.Out.WriteLine(e.Message);
+                    using (TextWriter streamWriter = new StreamWriter(errorFileName))
+                    {
+                        streamWriter.Write(taskIndex);
+                    }
+                    return -1;
+                }
+
+                pages = doc.SelectNodes("//page");
+                index = 1;
+                foreach (XmlNode page in pages)
+                {
+                    XmlNode category = page.SelectSingleNode("categories/cl");
+                    if (category != null)
+                    {
+                        continue;
+                    }
+                    string pageTitle = page.Attributes["title"].Value;
+                    Console.Out.WriteLine(string.Format("Processing '{0}' ({1}/{2})...",
+                        pageTitle, index++, pages.Count));
+                    try
+                    {
+                        wiki.AppendTextToPage(pageTitle,
+                            "\n\n",
+                            "Сброс кеша нулевой правкой",
+                            MinorFlags.None,
+                            WatchFlags.Watch);
+                    }
+                    catch (WikiException e)
+                    {
+                        Console.Out.WriteLine(e.Message);
+                        using (TextWriter streamWriter = new StreamWriter(errorFileName))
+                        {
+                            streamWriter.Write(taskIndex);
+                        }
+                        return -1;
                     }
                 }
             }
 
-            parameters.Clear();
-            parameters.Add("generator", "embeddedin");
-            parameters.Add("geititle", "Шаблон:Orphaned-fairuse");
-            parameters.Add("geilimit", "max");
-            parameters.Add("geinamespace", "6");
-            parameters.Add("prop", "categories");
-            parameters.Add("clcategories", "Категория:Файлы:Неиспользуемые несвободные:Просроченные подведения итогов");
-
-            try
+            if (File.Exists(errorFileName))
             {
-                doc = wiki.Enumerate(parameters, true);
-            }
-            catch (WikiException e)
-            {
-                Console.Out.WriteLine(e.Message);
-                return;
+                File.Delete(errorFileName);
             }
 
-            pages = doc.SelectNodes("//page");
-            index = 1;
-            foreach (XmlNode page in pages)
-            {
-                XmlNode category = page.SelectSingleNode("categories/cl");
-                if (category != null)
-                {
-                    continue;
-                }
-                string pageTitle = page.Attributes["title"].Value;
-                Console.Out.WriteLine(string.Format("Processing '{0}' ({1}/{2})...",
-                    pageTitle, index++, pages.Count));
-                try
-                {
-                    wiki.AppendTextToPage(pageTitle,
-                        "\n\n",
-                        "Сброс кеша нулевой правкой",
-                        MinorFlags.None,
-                        WatchFlags.Watch);
-                }
-                catch (WikiException e)
-                {
-                    Console.Out.WriteLine(e.Message);
-                    break;
-                }
-            }
-
-            parameters.Clear();
-            parameters.Add("generator", "categorymembers");
-            parameters.Add("gcmtitle", "Категория:Файлы:Перенесённые ботом");
-            parameters.Add("gcmlimit", "max");
-            parameters.Add("gcmnamespace", "6");
-            parameters.Add("prop", "categories");
-            parameters.Add("clcategories", "Категория:Файлы:Перенесённые на Викисклад");
-
-            try
-            {
-                doc = wiki.Enumerate(parameters, true);
-            }
-            catch (WikiException e)
-            {
-                Console.Out.WriteLine(e.Message);
-                return;
-            }
-
-            pages = doc.SelectNodes("//page");
-            index = 1;
-            foreach (XmlNode page in pages)
-            {
-                XmlNode category = page.SelectSingleNode("categories/cl");
-                if (category != null)
-                {
-                    continue;
-                }
-                string pageTitle = page.Attributes["title"].Value;
-                Console.Out.WriteLine(string.Format("Processing '{0}' ({1}/{2})...",
-                    pageTitle, index++, pages.Count));
-                try
-                {
-                    wiki.AppendTextToPage(pageTitle,
-                        "\n\n",
-                        "Сброс кеша нулевой правкой",
-                        MinorFlags.None,
-                        WatchFlags.Watch);
-                }
-                catch (WikiException e)
-                {
-                    Console.Out.WriteLine(e.Message);
-                    break;
-                }
-            }
             Console.Out.WriteLine("Done.");
+            return 0;
         }
     }
 }
